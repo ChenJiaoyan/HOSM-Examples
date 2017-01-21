58aae3f7156c7d5bca400c2086540ea1d26f8cdd
John <John@vpn131a.rzuser.uni-heidelberg.de>
2017-01-21 16:51:51 +0100

    sync code

--- src/main/java/org/heigit/hosm/example/MLib_Examples.java
+++ src/main/java/org/heigit/hosm/example/MLib_Examples.java
@@ -16,7 +16,7 @@ import java.util.Map;
 public class MLib_Examples {
     public static void main(String args []) throws IgniteCheckedException, ParseException, com.vividsolutions.jts.io.ParseException {
         String tagKey = "building";
-        String tagValue = "school";
+        String tagValue = "roof";
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
         String start_time_str = "20120101";
         String end_time_str = "20170101";
