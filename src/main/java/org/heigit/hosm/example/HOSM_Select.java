f560783ce2fdaf4239b14eab4bad6d9a29ecff5f
John <John@vpn165a.rzuser.uni-heidelberg.de>
2017-03-05 16:53:19 +0100

    HOSM_Shops

--- src/main/java/org/heigit/hosm/example/HOSM_Select.java
+++ src/main/java/org/heigit/hosm/example/HOSM_Select.java
@@ -39,22 +39,14 @@ public class HOSM_Select {
         private static final long serialVersionUID = 1L;
         private final List<Long> timestamps;
         private final Geometry bbox;
-        private final int tagKey;
-        private final int tagValue;
+        private final ArrayList<int[]> tag_ids;
 
-        public JobOption(final List<Long> timestamps, final Geometry bbox, final int tagKey) {
+        public JobOption(final List<Long> timestamps, final Geometry bbox, final ArrayList<int[]> tag_ids) {
             this.timestamps = timestamps;
             this.bbox = bbox;
-            this.tagKey = tagKey;
-            this.tagValue = -1;
+            this.tag_ids = tag_ids;
         }
 
-        public JobOption(final List<Long> timestamps, final Geometry bbox, final int tagKey, final int tagValue) {
-            this.timestamps = timestamps;
-            this.bbox = bbox;
-            this.tagKey = tagKey;
-            this.tagValue = tagValue;
-        }
     }
 
     public static class JobResult implements Serializable {
@@ -127,14 +119,11 @@ public class HOSM_Select {
                     for (Map.Entry<Long, OSMWay> timestampWay : timestampWayMap.entrySet()) {
                         Long timestamp = timestampWay.getKey();
                         OSMWay way = timestampWay.getValue();
-                        if (hasKeyValue(way.getTags(), option.tagKey, option.tagValue)) {
-                            //double lat = way.getLatitude() * GEOM_PRECISION;
-                            //double lon = way.getLongitude() * GEOM_PRECISION;
-                            int[] tags = way.getTags();
-                            String tags_s = tags2string(tags);
-                            String s = way.toString();
-                            String node_id = s.split(" ")[1].split(":")[1];
-                            s = String.format("way,%s,,,%s", node_id, tags_s);
+                        int[] way_tags = way.getTags();
+                        if (hasKeyValue(way_tags, option.tag_ids)) {
+                            String tags_s = tags2string(way_tags);
+                            String node_id = way.toString().split(" ")[1].split(":")[1];
+                            String s = String.format("way,%s,,,%s", node_id, tags_s);
                             if (result.containsKey(timestamp)) {
                                 ArrayList<String> r = result.get(timestamp);
                                 r.add(s);
@@ -168,14 +157,13 @@ public class HOSM_Select {
                         Long timestamp = timestampNode.getKey();
                         OSMNode node = timestampNode.getValue();
 
-                        if (hasKeyValue(node.getTags(), option.tagKey, option.tagValue)) {
+                        if (hasKeyValue(node.getTags(), option.tag_ids)) {
                             double lat = node.getLatitude() * GEOM_PRECISION;
                             double lon = node.getLongitude() * GEOM_PRECISION;
                             int[] tags = node.getTags();
                             String tags_s = tags2string(tags);
-                            String s = node.toString();
-                            String node_id = s.split(" ")[1].split(":")[1];
-                            s = String.format("node,%s,%f,%f,%s", node_id, lat, lon, tags_s);
+                            String node_id = node.toString().split(" ")[1].split(":")[1];
+                            String s = String.format("node,%s,%f,%f,%s", node_id, lat, lon, tags_s);
                             if (result.containsKey(timestamp)) {
                                 ArrayList<String> r = result.get(timestamp);
                                 r.add(s);
@@ -216,43 +204,39 @@ public class HOSM_Select {
         /*
         * tags is an index array of [key,value, key,value, ...] order by key!
         */
-        private boolean hasKeyValue(int[] tags, int key, int value) {
-            for (int i = 0; i < tags.length; i += 2) {
-                if (tags[i] < key)
-                    continue;
-                if (tags[i] == key) {
-                    if (value == -1) {
-                        return true;
-                    } else {
-                        if (tags[i + 1] == value) {
+        private boolean hasKeyValue(int[] tags, ArrayList<int[]> tag_ids) {
+            for (int[] tag_id : tag_ids) {
+                for (int i = 0; i < tags.length; i += 2) {
+                    if (tags[i] == tag_id[0]) {
+                        if (tag_id[1] == -1) {
                             return true;
                         } else {
-                            return false;
+                            if (tags[i + 1] == tag_id[1]) {
+                                return true;
+                            } else {
+                                return false;
+                            }
                         }
                     }
+                    return false;
                 }
-                return false;
             }
             return false;
         }
     }
 
 
-
-    public Map<Long, ArrayList<String>> spatial_temporal_select(String tagKey, String tagValue, ArrayList<Long> times_arr,String polygon_str,
+    public Map<Long, ArrayList<String>> spatial_temporal_select(String[] tags, ArrayList<Long> times_arr, String polygon_str,
                                                                 String[] obj_types) throws ParseException, com.vividsolutions.jts.io.ParseException, IgniteCheckedException {
         Ignition.setClientMode(true);
         IgniteConfiguration icfg = IgnitionEx.loadConfiguration("ignite.xml").getKey();
         try (Ignite ignite = Ignition.start(icfg)) {
-            int[] tags = get_tag_value(ignite, tagKey, tagValue);
-            System.out.println(tags);
-            if (tags != null) {
-                int tag_k_n = tags[0];
-                int tag_v_n = tags[1];
+            ArrayList<int[]> tag_ids = get_tag_id(ignite, tags);
+            if (tag_ids != null && tag_ids.size() > 0) {
                 List<Long> timestamps = times_arr;
                 WKTReader r = new WKTReader();
                 Geometry bbox = r.read(polygon_str);
-                JobOption option = new JobOption(timestamps, bbox, tag_k_n, tag_v_n);
+                JobOption option = new JobOption(timestamps, bbox, tag_ids);
                 IgniteCompute compute = ignite.compute(ignite.cluster().forRemotes());
                 SelectJob myJob = new SelectJob(option, ignite, false, obj_types);
                 JobResult result = (JobResult) myJob.execute();
@@ -264,35 +248,40 @@ public class HOSM_Select {
     }
 
 
-    private int[] get_tag_value(Ignite ignite, String tagKey, String tagValue) {
-        int tag_v_n = -1;
+    private ArrayList<int[]> get_tag_id(Ignite ignite, String[] tags) {
+        ArrayList<int[]> tag_ids = new ArrayList<>();
         IgniteCache<Integer, OSMTag> cacheTags = ignite.cache("osm_tags");
-        List<List<?>> rows = cacheTags
-                .query(new SqlFieldsQuery("select _key,values from OSMTag where key = ?").setArgs(tagKey)).getAll();
-        if (rows == null || rows.isEmpty()) {
-            System.err.println("Tags with key building not found!");
-            return null;
-        }
-        int tag_k_n = ((Integer) rows.get(0).get(0)).intValue();
-        System.out.printf("tag key: %d \n", tag_k_n);
+        for (String tag : tags) {
+            String key = tag.split(":")[0];
+            List<List<?>> rows = cacheTags
+                    .query(new SqlFieldsQuery("select _key,values from OSMTag where key = ?").setArgs(key)).getAll();
+            if (rows == null || rows.isEmpty()) {
+                continue;
+            }
+            int key_id = ((Integer) rows.get(0).get(0)).intValue();
+            Object[] values = (Object[]) rows.get(0).get(1);
 
-        Object[] values = (Object[]) rows.get(0).get(1);
-        for (int i = 0; i < values.length; i++) {
-            if (((String) values[i]).equals(tagValue)) {
-                tag_v_n = i;
-                break;
+            if (tag.split(":").length > 1) {
+                String value = tag.split(":")[1];
+                String[] value_split = tag.split(":")[1].split(",");
+                Arrays.sort(value_split);
+                for (int i = 0; i < values.length; i++) {
+                    String value_i = (String) values[i];
+                    if (Arrays.binarySearch(value_split, value_i) >= 0) {
+                        tag_ids.add(new int[]{key_id, i});
+                    }
+                }
+            } else {
+                tag_ids.add(new int[]{key_id, -1});
             }
         }
-        System.out.printf("%s \n", Arrays.toString((Object[]) rows.get(0).get(1)));
-        System.out.printf("tag value: %d \n", tag_v_n);
-
-        return new int[]{tag_k_n, tag_v_n};
+        return tag_ids;
     }
 
     private void save2file(File f, ArrayList<Long> times, Map<Long, ArrayList<String>> results) throws IOException {
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
-        for(int i=0; i<times.size();i++){
+        for (int i = 0; i < times.size(); i++) {
             long t = times.get(i);
             ArrayList<String> rs = results.get(t);
             Date resultdate = new Date(t);
@@ -301,7 +290,7 @@ public class HOSM_Select {
             File tf = new File(fname);
             tf.createNewFile();
             FileWriter fileWriter = new FileWriter(tf);
-            for(String r:rs){
+            for (String r : rs) {
                 fileWriter.write(r + "\n");
             }
             fileWriter.close();
@@ -312,15 +301,14 @@ public class HOSM_Select {
     public static void main(String args[]) throws ParseException, IgniteCheckedException, com.vividsolutions.jts.io.ParseException, IOException {
         String dir = args[0];
         File f = new File(dir);
-        if(f.exists()){
+        if (f.exists()) {
             System.out.printf("the output exist \n");
             System.exit(0);
-        }else{
+        } else {
             f.mkdir();
         }
 
-
-        String [] tags = new String [] {"shop","building:commercial"};
+        String[] tags = new String[]{"shop", "building:commercial"};
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
         String start_time_str = "20100101";
@@ -339,14 +327,13 @@ public class HOSM_Select {
 
         String polygon_str = "POLYGON((117.762451171875 32.12154573409534,122.530517578125 32.12154573409534," +
                 "122.530517578125 28.199742006199717,117.762451171875 28.199742006199717,117.762451171875 32.12154573409534))";
-        String[] obj_types = new String[]{"node","way"};
+        String[] obj_types = new String[]{"node", "way"};
 
         System.out.println("#### select the objects with tags: '" + Arrays.toString(tags) + "' #####");
         HOSM_Select client = new HOSM_Select();
-        System.out.println(tags[0]);
-        Map<Long, ArrayList<String>> results = client.spatial_temporal_select(tags[0], null, times, polygon_str, obj_types);
+        Map<Long, ArrayList<String>> results = client.spatial_temporal_select(tags, times, polygon_str, obj_types);
 
-        client.save2file(f,times,results);
+        client.save2file(f, times, results);
     }
 
 
